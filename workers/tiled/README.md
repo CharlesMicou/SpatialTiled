24a970e09ffc62258ccc7a1cbebbe44f9a72cdf2
Charles Micou <charlesmicou@gmail.com>
2018-06-07 21:28:01 +0100

    Add Java worker skeleton

---
+++ workers/tiled/README.md
@@ -0,0 +1,3 @@
+`spatial worker build` should generate SDK-based dependencies.
+
+Running this requires Gradle to be installed.
---
+++ workers/tiled/worker/src/main/java/DummyWorkerMain.java
@@ -0,0 +1,8 @@
+package dummy;
+
+public class DummyWorkerMain {
+    public static void main(String[] args) {
+        // Do nothing
+        System.exit(0);
+    }
+}
