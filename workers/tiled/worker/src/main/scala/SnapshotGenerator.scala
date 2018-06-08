334b63e439d2fece2045062d03dc8b93662e5f80
Charles Micou <charlesmicou@gmail.com>
2018-06-08 23:15:20 +0100

    Add Scala build setup and test generating a snapshot with it

---
+++ workers/tiled/worker/src/main/scala/SnapshotGenerator.scala
@@ -0,0 +1,53 @@
+package tiled.tiled;
+
+import java.util
+
+import improbable._
+import improbable.worker.{Entity, EntityId, SnapshotOutputStream}
+import tiled.map._
+import tiled.resource.TileResource
+
+import scala.collection.JavaConversions._
+
+object SnapshotGenerator extends App {
+    val path = System.getProperty("user.dir") + "/test.snapshot"
+    System.out.println(s"Outputting snapshot to $path")
+    val snapshotOutputStream = new SnapshotOutputStream(path)
+    snapshotOutputStream.writeEntity(new EntityId(1337), makeTestEntity)
+    snapshotOutputStream.close()
+    System.exit(0)
+
+    def makeTestEntity: Entity = {
+        val entity = new Entity()
+        entity.add(Metadata.COMPONENT, new MetadataData("ScalaGeneratedEntity"))
+        entity.add(EntityAcl.COMPONENT, makeEntityAcl)
+        entity.add(Persistence.COMPONENT, new PersistenceData())
+        entity.add(Position.COMPONENT, new PositionData(new Coordinates(1, 0, -1)))
+        entity.add(MapChunk.COMPONENT, makeMapChunkData)
+        entity
+    }
+
+    def makeEntityAcl: EntityAclData = {
+        val readAcl = new WorkerRequirementSet(new java.util.ArrayList())
+        val writeAcl = new util.HashMap[Integer, WorkerRequirementSet]()
+        new EntityAclData(readAcl, writeAcl)
+    }
+
+    def makeMapChunkData: MapChunkData = {
+        val properties = MapChunkProperties.create()
+        properties.setName("Test map name")
+        properties.setHeight(2)
+        properties.setWidth(3)
+        properties.setTileXDimension(1)
+        properties.setTileZDimension(1)
+        val tileLayer = TileLayer.create()
+        tileLayer.setName("tile layer 1")
+        tileLayer.setTiles(Seq(new TileId(1, 1), new TileId(1, 2)))
+        val tileResource = TileResource.create()
+        tileResource.setResourceId(1)
+        new MapChunkData(
+            properties,
+            Map((1: Integer) -> tileResource),
+            Seq(tileLayer))
+    }
+}
