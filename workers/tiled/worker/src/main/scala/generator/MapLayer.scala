4eec0d7ec03d499a9bd8a758f4d2e1503b9ec9e3
Charles Micou <charlesmicou@gmail.com>
2018-06-24 19:07:01 +0100

    Fix clipping bug in map layer merge

--- workers/tiled/worker/src/main/scala/generator/MapLayer.scala
+++ workers/tiled/worker/src/main/scala/generator/MapLayer.scala
@@ -1,26 +1,32 @@
 package generator
 
 import common.CoordinatesHelper._
+import common.MagicConstants.{TILE_X_DIMENSION, TILE_Z_DIMENSION}
 import common.{GridMap, MagicConstants}
 import improbable.Coordinates
 
 case class MapLayer(name: String, id: Int, tileData: GridMap)
 
 object MapLayer {
+    /** Merge several map layers with coordinate offsets into a single map layer */
     def merge(parts: Map[Coordinates, MapLayer]): MapLayer = {
         val name = parts.head._2.name
         val id = parts.head._2.id
 
         val topLeft = parts.keys.toSeq.sortWith((c1, c2) => c1.isTopLeftOf(c2)).head
-        val bottomRight = parts.keys.toSeq.sortWith((c1, c2) => c1.isBottomRightOf(c2)).head
+        val bottomRightPair = parts.toSeq.sortWith((f1, f2) => f1._1.isBottomRightOf(f2._1)).head
+        val bottomRight = bottomRightPair._1 + makeCoordinates(
+            TILE_X_DIMENSION * bottomRightPair._2.tileData.width,
+            0,
+            TILE_Z_DIMENSION * bottomRightPair._2.tileData.height)
 
-        val width: Int = ((bottomRight.getX - topLeft.getX) / MagicConstants.TILE_X_DIMENSION).toInt
-        val height: Int = ((topLeft.getZ - bottomRight.getZ) / MagicConstants.TILE_X_DIMENSION).toInt
+        val width: Int = ((bottomRight.getX - topLeft.getX) / TILE_X_DIMENSION).toInt
+        val height: Int = ((topLeft.getZ - bottomRight.getZ) / TILE_Z_DIMENSION).toInt
 
         val mergedGridData = parts.flatMap(pair => {
             val relativeOrigin = pair._1 - topLeft
-            val xGridOffset = (relativeOrigin.getX / MagicConstants.TILE_X_DIMENSION).toInt
-            val zGridOffset = -(relativeOrigin.getZ / MagicConstants.TILE_Z_DIMENSION).toInt
+            val xGridOffset = (relativeOrigin.getX / TILE_X_DIMENSION).toInt
+            val zGridOffset = -(relativeOrigin.getZ / TILE_Z_DIMENSION).toInt
 
             pair._2.tileData.data.map(f => {
                 (xGridOffset + f._1._1, zGridOffset + f._1._2) -> f._2
