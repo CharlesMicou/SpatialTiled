22aa25c805cbce2fa8ce1635526bd02a229eccc2
Charles Micou <charlesmicou@gmail.com>
2018-08-04 18:09:49 +0100

    Add a real map and move the validation map into its own folder.

---
+++ worlds/test_world/tilesets/spring.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="spring" tilewidth="64" tileheight="64" tilecount="14" columns="7">
+ <image source="../img/spring.png" width="448" height="128"/>
+ <tile id="10">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
